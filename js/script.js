035143746698c1e487b391b109513f95a60eb1f3
Karol Niedźwiecki <niedzwiecki7@gmail.com>
2021-07-31 13:39:21 +0200

    Change script.js add class active for clicked element

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
@@ -13,6 +14,8 @@ for(let activeLink of activeLinks){
 }
 
   /* add class 'active' to the clicked link */
+clickedElement.classList.add('active');
+console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('article.active');
