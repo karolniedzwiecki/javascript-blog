2aefa16542f2b0a4cdf9e81fe645d14c4ccf23af
Karol Niedźwiecki <niedzwiecki7@gmail.com>
2021-08-10 13:03:30 +0200

    Modify function generateTags

--- js/script.js
+++ js/script.js
@@ -36,7 +36,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = ".tags.list";
 
 function generateTitleLinks(customSelector = ''){
 
@@ -76,6 +77,8 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
@@ -96,6 +99,11 @@ function generateTags(){
       const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
       /* add generated code to html variable */
       html = html + " " + linkHTML;
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     /* END LOOP: for each tag */
   }
     /* insert HTML of all the links into the tags wrapper */
@@ -106,9 +114,15 @@ function generateTags(){
 
     for(let link of links){
       link.addEventListener('click', titleClickHandler);
-}
-  /* END LOOP: for every article: */
-}
+    }
+    /* END LOOP: for every article: */
+  }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+      /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
+
 }
 
 generateTags();
